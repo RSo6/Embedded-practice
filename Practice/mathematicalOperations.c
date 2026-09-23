5c88b401a9e4ed2b71c542c594210d779f4db280
Roman Shnep <romansnep79@gmail.com>
2026-09-23 19:01:08 +0200

    feat(practice): implement mathematical equation calculations

---
+++ Practice/mathematicalOperations.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <errno.h>
+
+
+double firstEquationCalc(double);
+double secEquationCalc(double);
+
+int main() {
+    double x = 3.3;
+    while (1) {
+        double const first_equation = firstEquationCalc(x);
+        double const sec_equation = secEquationCalc(x);
+        printf("First Equation result --> %.5lf\n", first_equation);
+        printf("Second Equation result --> %.5lf\n", sec_equation);
+
+        if (first_equation == EDOM || sec_equation == EDOM)
+            printf("Calculation failed, the error message is --> %s\n", strerror(EDOM));
+
+        printf("\nInsert the value to calculate by equations -->  ");
+
+        if (scanf("%lf", &x) != 1) {
+            printf("root\n");
+            break;
+        }
+    }
+    return 0;
+}
+
+double firstEquationCalc(double x) {
+    double root = x * x - 9;
+    if (root < 0.0) return EDOM;
+    double const numerator = (x * x) + 2 * x - 3 + (x + 1) * sqrt(root);
+    double const denominator = (x * x) - 2 * x - 3 + (x - 1) * sqrt(root);
+    return denominator == 0.0 ? EDOM : numerator / denominator;
+}
+
+double secEquationCalc(double x) {
+    double const root = (x + 3) / (x - 3);
+    if (root < 0.0) return EDOM;
+    return (x - 3) == 0.0 ? EDOM : sqrt((x + 3) / (x - 3));
+}
+
